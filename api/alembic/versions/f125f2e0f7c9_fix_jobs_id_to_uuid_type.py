4f905f9732a0cc2e5b3b80023f4fcb856207e8ff
mrodek <martin_rodek@yahoo.com>
2026-01-14 10:50:50 -0500

    fix: properly named migration to convert jobs.id to UUID

--- api/alembic/versions/f125f2e0f7c9_fix_jobs_id_to_uuid_type.py
+++ api/alembic/versions/f125f2e0f7c9_fix_jobs_id_to_uuid_type.py
@@ -1,22 +1,24 @@
-"""fix jobs.id to UUID type
+"""fix_jobs_id_to_uuid_type
 
-Revision ID: fix_jobs_id_uuid
-Revises: 29cfa1f992ef
-Create Date: 2026-01-14
+Revision ID: f125f2e0f7c9
+Revises: fix_jobs_id_uuid
+Create Date: 2026-01-14 10:49:49.197425
 
 """
+from typing import Sequence, Union
+
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
+
 
 # revision identifiers, used by Alembic.
-revision = 'fix_jobs_id_uuid'
-down_revision = '29cfa1f992ef'
-branch_labels = None
-depends_on = None
+revision: str = 'f125f2e0f7c9'
+down_revision: Union[str, None] = '29cfa1f992ef'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 
-def upgrade():
+def upgrade() -> None:
     # Convert jobs.id from VARCHAR to UUID
     # The USING clause tells PostgreSQL how to convert the existing VARCHAR values to UUID
     op.execute('ALTER TABLE jobs ALTER COLUMN id TYPE UUID USING id::uuid')
@@ -25,7 +27,7 @@ def upgrade():
     op.execute('ALTER TABLE saved_jobs ALTER COLUMN job_id TYPE UUID USING job_id::uuid')
 
 
-def downgrade():
+def downgrade() -> None:
     # Convert back to VARCHAR if needed
     op.execute('ALTER TABLE jobs ALTER COLUMN id TYPE VARCHAR USING id::text')
     op.execute('ALTER TABLE saved_jobs ALTER COLUMN job_id TYPE VARCHAR USING job_id::text')
